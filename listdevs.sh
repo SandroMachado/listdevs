edeeb4db6d92f0efda5b30e7283a66414711f31d
Sandro Machado <sandroemachado@gmail.com>
2014-05-20 19:31:58 +0100

    Added the ability to directly verify if a UDID is present

--- listdevs.sh
+++ listdevs.sh
@@ -7,11 +7,25 @@
 LANG=C
 LC_CTYPE=C
 IPAFILE="$1"
+UDID="$2"
 TMPDIR=tmp$$.tmp
 mkdir $TMPDIR
 unzip -qq "$IPAFILE" -d $TMPDIR
 EMBEDFILE=`find $TMPDIR -name embedded.mobileprovision -print | head -1`
 EMBEDFILEONLY="${EMBEDFILE}.plist"
 sed -n '/plist/,/\/plist/p' "$EMBEDFILE" > "$EMBEDFILEONLY"
-sed -n '/ProvisionedDevices/,/\/array/p' "$EMBEDFILEONLY" | grep string | sed 's/<[/]*string>//g'
+UDIDs=$(sed -n '/ProvisionedDevices/,/\/array/p' "$EMBEDFILEONLY" | grep string | sed 's/<[/]*string>//g')
 rm -rf $TMPDIR
+if [[ -z "$UDID" ]]; then
+	printf "\n*****UDID*******\n%s\n****************\n" "$UDIDs"
+	exit 1;
+fi 
+if [ ${#UDID} -ne 40 ]; then
+	printf "\nInvalid UDID: %s\n"  "$UDID"
+	exit 1;
+fi
+if [[ $UDIDs == *$UDID* ]] ; then
+	printf "UDID is present\xF0\x9f\x8d\xba\n" 
+else
+	printf "\nUDID is NOT present\n" 
+fi
